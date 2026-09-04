b5e1a9e7d580f3da3926268847fd5a23cc31e067
Piunifio <piunifio@gmail.com>
2026-09-04 00:28:08 +0000

    ex3

--- Projeto-Inicial/src/app/listaexerc/listaexerc-module.ts
+++ Projeto-Inicial/src/app/listaexerc/listaexerc-module.ts
@@ -2,9 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ListaexercRoutingModule } from './listaexerc-routing-module';
+import { Ex1 } from './ex1/ex1';
+import { Ex2 } from './ex2/ex2';
+import { Ex3 } from './ex3/ex3';
+import { Ex4 } from './ex4/ex4';
+import { Ex5 } from './ex5/ex5';
 
 @NgModule({
-  declarations: [],
+  declarations: [Ex1, Ex2, Ex3, Ex4, Ex5],
   imports: [CommonModule, ListaexercRoutingModule],
 })
 export class ListaexercModule {}
